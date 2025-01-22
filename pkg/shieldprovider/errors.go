e0aa41cb1a30c78c4b38dccf85c0a71cf773e473
rylenko <rylenko@tuta.io>
2025-01-23 04:18:36 +0700

    Reorder variables in alphabetical order

--- pkg/shieldprovider/errors.go
+++ pkg/shieldprovider/errors.go
@@ -3,7 +3,7 @@ package shieldprovider
 import "errors"
 
 var (
-	ErrInvalidValue         = errors.New("invalid value")
 	ErrConvertToForeignType = errors.New("convert to foreign type")
 	ErrDiffieHellman        = errors.New("Diffie-Hellman")
+	ErrInvalidValue         = errors.New("invalid value")
 )
