e7df1ea2193d9c30a3c16469ac231a94e5b4c72a
rylenko <rylenko@tuta.io>
2025-03-20 13:50:30 +0700

    Test header encoding and decoding

---
+++ pkg/ratchet/header/header_test.go
@@ -0,0 +1,98 @@
+package header
+
+import (
+	stderrors "errors"
+	"slices"
+	"testing"
+
+	"github.com/rylenko/bastion/pkg/ratchet/errors"
+	"github.com/rylenko/bastion/pkg/ratchet/keys"
+)
+
+func TestHeaderSuccessEncodeAndDecode(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		header Header
+		bytes  []byte
+	}{
+		{
+			Header{
+				PublicKey:                         keys.Public{Bytes: []byte{0x01, 0x02, 0x03, 0x04, 0x05}},
+				PreviousSendingChainMessagesCount: 123,
+				MessageNumber:                     321,
+			},
+			[]byte{
+				0x41, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+				0x7b, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+				0x01, 0x02, 0x03, 0x04, 0x05,
+			},
+		},
+		{
+			Header{
+				PublicKey:                         keys.Public{Bytes: nil},
+				PreviousSendingChainMessagesCount: 0,
+				MessageNumber:                     0,
+			},
+			[]byte{
+				0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+				0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
+			},
+		},
+	}
+
+	for _, test := range tests {
+		bytes := test.header.Encode()
+		if !slices.Equal(bytes, test.bytes) {
+			t.Fatalf("%+v.Encode(): expected %v but got %v", test.header, test.bytes, bytes)
+		}
+
+		header, err := Decode(bytes)
+		if err != nil {
+			t.Fatalf("Decode(%v): expected no error but got %v", bytes, err)
+		}
+
+		if !slices.Equal(header.PublicKey.Bytes, test.header.PublicKey.Bytes) {
+			t.Fatalf("Decode(%v): invalid public key: %v != %v", bytes, header.PublicKey.Bytes, test.header.PublicKey.Bytes)
+		}
+
+		if header.PreviousSendingChainMessagesCount != test.header.PreviousSendingChainMessagesCount {
+			t.Fatalf(
+				"Decode(%v): invalid previous sending chain message count: %v != %v",
+				bytes,
+				header.PreviousSendingChainMessagesCount,
+				test.header.PreviousSendingChainMessagesCount,
+			)
+		}
+
+		if header.MessageNumber != test.header.MessageNumber {
+			t.Fatalf("Decode(%v): invalid message number: %v != %v", bytes, header.MessageNumber, test.header.MessageNumber)
+		}
+	}
+}
+
+func TestHeaderDecodeError(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		bytes         []byte
+		errorCategory error
+		errorString   string
+	}{
+		{
+			[]byte{
+				0x12, 0x00, 0x00, 0x00, 0x22, 0x00, 0x00, 0x0F,
+				0x55, 0x00, 0x00, 0x00, 0x77, 0x00, 0x0B,
+			},
+			errors.ErrInvalidValue,
+			"invalid value: not enough bytes",
+		},
+		{nil, errors.ErrInvalidValue, "invalid value: not enough bytes"},
+	}
+
+	for _, test := range tests {
+		if _, err := Decode(test.bytes); !stderrors.Is(err, test.errorCategory) || err.Error() != test.errorString {
+			t.Fatalf("Decode(%v) expected error %q but got %v", test.bytes, test.errorString, err)
+		}
+	}
+}
